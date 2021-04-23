78a848c992797539c316fcb238039855ca15d5ee
github username <github email address>
2021-04-22 20:13:24 -0400

    commit tree 0a62abfe250eb251eebca3cb1f8447a6eb231c8b

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+function superbowlWin(objects) {
+    const w = objects.find(obj => obj.result === "W")
+    if (w && w.year) {
+        return w.year
+    } else {
+        return undefined
+    }
+}
 
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ] 
+
+  superbowlWin(record)
